dc870e9cf147271ebcb674fdfe6ed8570caf2c32
rootrc <rootrc123@gmail.com>
2026-01-13 16:12:38 -0500

    added pages and test graph

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import DFS from "./pages/DFS";
+
 export default function App() {
   return (
-    <p>Hello, World!</p>
+    <>
+      <nav>
+        <Link to="/">Home</Link>{" "}
+        <Link to="/dfs">DFS</Link>{" "}
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dfs" element={<DFS />} />
+      </Routes>
+    </>
   )
 }
